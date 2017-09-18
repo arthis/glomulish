ee0f1a02b328e407da937abb3783874ad0689b66
yoann <yoann@fake.local>
2017-09-18 15:17:09 +0200

    initial Commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM microsoft/dotnet:2.0-sdk
+
+
+# copy and build everything
+ADD deploy ./app
+
+WORKDIR app
+
+
+EXPOSE 8080
+EXPOSE 8085
+ENTRYPOINT ["dotnet", "Server.dll"]
---
+++ build.fsx
@@ -0,0 +1,230 @@
+// --------------------------------------------------------------------------------------
+// FAKE build script
+// --------------------------------------------------------------------------------------
+
+#r @"packages/build/FAKE/tools/FakeLib.dll"
+
+open Fake
+open Fake.Git
+open Fake.AssemblyInfoFile
+open Fake.ReleaseNotesHelper
+open System
+open System.IO
+open Fake.Testing.Expecto
+
+let project = "Glomulish"
+
+let summary = "Glomulishing is the key!"
+
+let description = summary
+
+let configuration = "Release"
+let glomulishPath = "./src/Hosts.Batch" |> FullName
+
+let serverTestsPath = "./test/ServerTests" |> FullName
+let clientTestsPath = "./test/UITests" |> FullName
+
+let dotnetcliVersion = "2.0.0"
+
+let mutable dotnetExePath = "dotnet"
+
+let deployDir = "./deploy"
+
+// Pattern specifying assemblies to be tested using expecto
+let clientTestExecutables = "test/UITests/**/bin/**/*Tests*.exe"
+
+let dockerUser = "yremy"
+let dockerImageName = "glomulish"
+
+let run' timeout cmd args dir =
+    if execProcess (fun info ->
+        info.FileName <- cmd
+        if not (String.IsNullOrWhiteSpace dir) then
+            info.WorkingDirectory <- dir
+        info.Arguments <- args
+    ) timeout |> not then
+        failwithf "Error while running '%s' with args: %s" cmd args
+
+let run = run' System.TimeSpan.MaxValue
+
+let runDotnet workingDir args =
+    let result =
+        ExecProcess (fun info ->
+            info.FileName <- dotnetExePath
+            info.WorkingDirectory <- workingDir
+            info.Arguments <- args) TimeSpan.MaxValue
+    if result <> 0 then failwithf "dotnet %s failed" args
+
+let platformTool tool winTool =
+    let tool = if isUnix then tool else winTool
+    tool
+    |> ProcessHelper.tryFindFileOnPath
+    |> function Some t -> t | _ -> failwithf "%s not found" tool
+
+let nodeTool = platformTool "node" "node.exe"
+let npmTool = platformTool "npm" "npm.cmd"
+let yarnTool = platformTool "yarn" "yarn.cmd"
+
+
+do if not isWindows then
+    // We have to set the FrameworkPathOverride so that dotnet sdk invocations know
+    // where to look for full-framework base class libraries
+    let mono = platformTool "mono" "mono"
+    let frameworkPath = IO.Path.GetDirectoryName(mono) </> ".." </> "lib" </> "mono" </> "4.5"
+    setEnvironVar "FrameworkPathOverride" frameworkPath
+
+
+// Read additional information from the release notes document
+let release =
+     ReadFile "RELEASE_NOTES.md"
+     |> ReleaseNotesHelper.parseReleaseNotes
+let packageVersion = SemVerHelper.parse release.NugetVersion
+
+let vboxManagePath  = "\"C:\\Program Files\\Oracle\\VirtualBox\\vBoxManage\""
+
+
+let startingVM nameVM (x: Diagnostics.ProcessStartInfo)  =
+    x.FileName <- vboxManagePath
+    x.Arguments <- sprintf "startvm %s --type headless" nameVM
+    ()
+
+let listingVm (x: Diagnostics.ProcessStartInfo)  = 
+    x.FileName <- vboxManagePath
+    x.Arguments <- "list runningvms"
+    ()  
+
+let startVM nameVM =
+    ExecProcessAndReturnMessages (startingVM nameVM) (TimeSpan.FromSeconds(float <| 30)) 
+    |> printfn "returns %A" 
+
+let isVmStarted nameVm =
+    let result = ExecProcessAndReturnMessages listingVm (TimeSpan.FromSeconds(float <| 30)) 
+    result.Messages
+    |> Seq.exists (fun x-> x.Contains(nameVm))
+    
+let docker instruction =
+    if not <| isVmStarted "default" then
+		startVM "default"
+
+    
+    PowerShell.Create()
+        .AddScript("$env:DOCKER_HOST=\"tcp://192.168.99.100:2376\"")
+        .AddStatement().AddScript("$env:DOCKER_CERT_PATH = \"C:\\Users\\remyy\\.docker\\machine\\machines\\default\"")
+        .AddStatement().AddScript("$env:DOCKER_TLS_VERIFY = 1")
+        .AddStatement().AddScript("$env:NO_PROXY=\"192.168.99.100\"")
+        .AddStatement().AddScript(sprintf "docker %s" instruction)
+        .Invoke()
+        
+     
+let isSuccessOr result=
+    result |> Seq.iter (printfn "Powershell result  : %O")
+    
+
+// --------------------------------------------------------------------------------------
+// Clean build results
+
+Target "Clean" (fun _ ->
+    !!"src/**/bin" ++ "src/**/obj/"
+        ++ "test/**/bin" ++ "test/**/obj/"
+    |> CleanDirs
+    CleanDirs ["bin"; "temp"; "docs/output"; deployDir; ]
+)
+
+Target "AssemblyInfo" (fun _ ->
+    CreateFSharpAssemblyInfo "src/Common/AssemblyInfo.fs"
+       [ Attribute.Title project
+         Attribute.Product project
+         Attribute.Description summary
+         Attribute.Version release.AssemblyVersion
+         Attribute.FileVersion release.AssemblyVersion]
+)
+
+Target "InstallDotNetCore" (fun _ ->
+    dotnetExePath <- DotNetCli.InstallDotNetSDK dotnetcliVersion
+)
+
+Target "Installglomulish" (fun _ ->
+    runDotnet glomulishPath "restore"
+)
+
+Target "Buildglomulish" (fun _ ->
+    runDotnet glomulishPath "build"
+)
+
+// --------------------------------------------------------------------------------------
+// Release Scripts
+
+Target "PrepareRelease" (fun _ ->
+    Git.Branches.checkout "" false "master"
+    Git.CommandHelper.directRunGitCommand "" "fetch origin" |> ignore
+    Git.CommandHelper.directRunGitCommand "" "fetch origin --tags" |> ignore
+
+    StageAll ""
+    Git.Commit.Commit "" (sprintf "Bumping version to %O" release.NugetVersion)
+    Git.Branches.pushBranch "" "origin" "master"
+
+    let tagName = string release.NugetVersion
+    Git.Branches.tag "" tagName
+    Git.Branches.pushTag "" "origin" tagName
+
+    sprintf "tag %s/%s %s/%s:%s" dockerUser dockerImageName dockerUser dockerImageName release.NugetVersion
+    |> docker 
+    |> isSuccessOr "Docker tag failed"
+    
+)
+
+Target "Publish" (fun _ ->
+    let result =
+        ExecProcess (fun info ->
+            info.FileName <- dotnetExePath
+            info.WorkingDirectory <- serverPath
+            info.Arguments <- "publish -c Release -o \"" + FullName deployDir + "\"") TimeSpan.MaxValue
+    if result <> 0 then failwith "Publish failed"
+
+    let clientDir = deployDir </> "client"
+    let publicDir = clientDir </> "public"
+    let jsDir = clientDir </> "js"
+    let cssDir = clientDir </> "css"
+    let imageDir = clientDir </> "Images"
+
+    !! "src/Client/public/**/*.*" |> CopyFiles publicDir
+    !! "src/Client/js/**/*.*" |> CopyFiles jsDir
+    !! "src/Client/css/**/*.*" |> CopyFiles cssDir
+    !! "src/Images/**/*.*" |> CopyFiles imageDir
+
+    "src/Client/index.html" |> CopyFile clientDir
+)
+
+Target "CreateDockerImage" (fun _ ->
+    sprintf "build -t %s/%s ." dockerUser dockerImageName
+    |> docker
+    |> isSuccessOr "Docker build failed"
+)
+
+Target "Deploy" (fun _ ->
+    // info.WorkingDirectory <- deployDir
+    //artissae/Cerf2017
+    sprintf "login --username \"%s\" --password \"%s\"" dockerUser (getBuildParam "DockerPassword")
+    |> docker
+    |> isSuccessOr "Docker login failed"
+    
+
+    // info.WorkingDirectory <- deployDir
+    sprintf "push %s/%s:%s" dockerUser dockerImageName release.NugetVersion
+    |> docker 
+    |> isSuccessOr "Docker push failed"
+)
+
+// -------------------------------------------------------------------------------------
+Target "All" DoNothing
+
+"Clean"
+    ==> "InstallDotNetCore"
+    ==> "Installglomulish"
+    ==> "Buildglomulish"
+    ==> "All"
+    ==> "PrepareRelease"
+    ==> "CreateDockerImage"
+
+
+RunTargetOrDefault "All"
